c7d80cde19246a2307c8688e66a4e8219384a4a6
gcenabore <gianenabore30@gmail.com>
2024-10-23 02:11:51 +0800

    defining valid criteria

--- user_info.py
+++ user_info.py
@@ -5,3 +5,16 @@
 #ask user if they want to continue, otherwise break the loop
 #assume the first entry is the oldest, if the current age is greater than the previous age, update the oldest person
 #print the name and age of the oldest person
+
+def is_valid_full_name(name):
+    return all(part.isalpha() for part in name.split()) and len(name) >0
+
+def is_valid_age(age):
+    return age.isdigit() and 0 <= int(age) <= 122
+
+def is_valid_number(num):
+    return num.isdigit() and len(num) == 11
+
+def main():
+    user_info = []
+    
