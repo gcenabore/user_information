f7c6f7911649a1b7a1c192a39f38089bb8fcf60f
gcenabore <gianenabore30@gmail.com>
2024-10-23 01:49:45 +0800

    pseudocode

---
+++ user_info.py
@@ -0,0 +1,7 @@
+#make an empty list to store the user information
+#define valid criteria for name and age
+#ask user for name, age, number, if invalid print an error message, otherwise continue.
+#store name and age using dictionary array and add to the list
+#ask user if they want to continue, otherwise break the loop
+#assume the first entry is the oldest, if the current age is greater than the previous age, update the oldest person
+#print the name and age of the oldest person
