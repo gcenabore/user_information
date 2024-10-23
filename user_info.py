1315b558cf3aff21fbae0ff35aabef9693bddd54
gcenabore <gianenabore30@gmail.com>
2024-10-23 14:24:01 +0800

    final

--- user_info.py
+++ user_info.py
@@ -5,7 +5,7 @@
 #ask user if they want to continue, otherwise break the loop
 #assume the first entry is the oldest, if the current age is greater than the previous age, update the oldest person
 #print the name and age of the oldest person
-
+    #This is to define valid criteria for name, age, and number.
 def is_valid_full_name(name):
     return all(part.isalpha() for part in name.split()) and len(name) >0
 
@@ -14,10 +14,10 @@ def is_valid_age(age):
 
 def is_valid_number(num):
     return num.isdigit() and len(num) == 11
-
+    #This to store the user information.
 def main():
     user_info = []
-
+        #This is to ask user for the information, if invalid print error, otherwise continue.
     while True:
         name = input("Enter your Full Name: ")
         if not is_valid_full_name(name):
@@ -34,24 +34,24 @@ def main():
             print("Error: Invalid number. Please input 11 digit number.")
             continue
 
-        user_information = {
+        user_information = { #This is to store name, age, number.
             "name": name,
             "age": int(age),
             "phone number": num
         }
-        user_info.append(user_information)
-
+        user_info.append(user_information) #This is to add another entry.
+            #This is for asking the user for another entry.
         another_entry = input("Do you want to enter another entry? (Yes/No): ").strip().lower()
         if another_entry == 'no':
             break
-         
+        #This is to fine who is the oldest person in the List.
     if user_info:
         oldest_user = max(user_info, key=lambda user_info: user_info['age'])
         print(f"The oldest person in the List is {oldest_user['name']} with an age of {oldest_user['age']} and Phone Number {oldest_user['phone number']}.")
         print("Note: The information you provided will remain safe and secure within this code.")
-    
+        #This is to check if there are information entered.
     else:
         print("No user information was entered.")
-
+    #This is to run the code.
 if __name__ == '__main__':
     main()
